bb327bf62e9cd485a5fad36b38d4e880ede47070
Facundo Feliu <feliufacundo0@gmail.com>
2025-10-19 23:07:50 -0300

    cambio modelo resultado estudio

--- project/ResultadoEstudio/models.py
+++ project/ResultadoEstudio/models.py
@@ -4,7 +4,6 @@ from PrimerConsulta.models import PrimeraConsulta
 
 class ResultadoEstudio(models.Model):
     consulta = models.ForeignKey(PrimeraConsulta, on_delete=models.CASCADE, related_name="resultados_estudios")
-    estudio_id_remoto = models.IntegerField()  # ID del estudio en la BD remota
     nombre_estudio = models.CharField(max_length=100)  # opcional: para cache/local display
     tipo = models.CharField(max_length=50, null=True, blank=True)  # opciona
     valor = models.CharField(max_length=255, blank=True, null=True)
